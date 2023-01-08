a4ed45484bf999523afc31eb876756e330a131ab
ganeshardlkar <ganeshardalkar5@gmail.com>
2023-01-08 14:23:13 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+
+const cron = require('node-cron');
 
 const setupAndStartServer = () => {
     const app = express();
@@ -14,12 +16,16 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server started on ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'ardalkarg@gmail.com',
-            'This is a testing email',
-            'Hey! How are you. I hope you our testing service'
-        )
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'ardalkarg@gmail.com',
+        //     'This is a testing email',
+        //     'Hey! How are you. I hope you our testing service'
+        // )
+        
+        // cron.schedule('*/2 * * * *', () => {
+        //     console.log('Running a task every 2 minutes');
+        // });
     });
 }
 
